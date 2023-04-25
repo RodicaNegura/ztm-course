b9073f5c53c10f33f12f5d1e2d65d223db6da2e9
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-04-25 12:11:48 +0100

    signIn

--- facebook/js/script.js
+++ facebook/js/script.js
@@ -15,3 +15,15 @@ var newsFeed = [
     timeline: "Javascript is so cool!"
   }
 ];
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+  if (user === database[0].username && pass === database[0].password) {
+    console.log(newsFeed)
+  } else {
+    alert("Sorry, wrong username and password!");
+  }
+};
+
+signIn(userNamePrompt, passwordPrompt);
