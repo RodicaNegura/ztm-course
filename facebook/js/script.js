f596b437678e4914fa28c5599139bd7dde896b0e
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-04-25 11:57:05 +0100

    add some variable

---
+++ facebook/js/script.js
@@ -0,0 +1,17 @@
+var database = [
+  {
+    username: "Rodica",
+    password: "supersecret"
+  }
+];
+
+var newsFeed = [
+  {
+    username: "Elena",
+    timeline: "So tired from all this learning!"
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is so cool!"
+  }
+];
