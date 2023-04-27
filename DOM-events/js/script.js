c01bb1184dd6263fce5ca80052e86471442a1c0b
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-04-27 16:26:03 +0100

    javascript

--- DOM-events/js/script.js
+++ DOM-events/js/script.js
@@ -1,5 +1,22 @@
-var button = document.getElementsByTagName("button")[0];
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
-  console.log("CLICK!!!");
+  if(input.value.length > 0) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+  }
 })
+
+input.addEventListener("keypress", function(event) {
+  if(input.value.length > 0 && event.code === "Enter") {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+  }
+})
+  
