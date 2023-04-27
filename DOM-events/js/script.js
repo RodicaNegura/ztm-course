3d59a1eb0703ba8549c9c3a7115fb7662937fc56
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-04-27 16:40:18 +0100

    added function in javascript

--- DOM-events/js/script.js
+++ DOM-events/js/script.js
@@ -2,21 +2,30 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-  if(input.value.length > 0) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-  }
-})
+function inputLength() {
+  return input.value.length;
+}
 
-input.addEventListener("keypress", function(event) {
-  if(input.value.length > 0 && event.code === "Enter") {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if(inputLength() > 0) {
+      createListElement();
+  } 
+}
+
+function addListAfterKeypress(event) {
+  if(inputLength() > 0 && event.code === "Enter") {
+    createListElement();
   }
-})
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
   
